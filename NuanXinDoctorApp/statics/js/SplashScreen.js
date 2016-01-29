b0e38b2fe1df15b52bd98984b518cbbacb5829e0
xwpfullstack <xwp_fullstack@163.com>
2016-01-29 19:22:13 +0800

    add NuanxinDoctor

---
+++ NuanXinDoctorApp/statics/js/SplashScreen.js
@@ -0,0 +1,26 @@
+'use strict'
+
+import React,{
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Component,
+} from 'react-native';
+
+class SplashScreen extends Component { 
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <View>
+        <Text>
+            123
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default SplashScreen;
